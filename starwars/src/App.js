c39c97a18312d10b1579782460354d12793b4729
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-19 09:20:57 -0700

    give app max-width, cards no longer stretched

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <Card.Group centered itemsPerRow={3} items={stubItems} />
+      <Card.Group centered items={stubItems} />
     </div>
   );
 }
